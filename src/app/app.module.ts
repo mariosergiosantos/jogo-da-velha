f6a14cac4eba3d9f7c781e3e7feeb3f252f91e9c
Mário Sérgio <mariosergio952@hotmail.com.br>
2018-04-19 22:56:51 -0300

    conteudo do curso da formação angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
